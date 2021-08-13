c90aaef259723677c5a134edb5f597e889fa1913
snufffbox <shraybiken@gmail.com>
2021-08-14 02:38:05 +0600

    react-2

--- react/react-2/src/components/FavouriteCities.tsx
+++ react/react-2/src/components/FavouriteCities.tsx
@@ -1,28 +1,32 @@
-import React, { Component, MouseEvent } from 'react'
+import React, { Component, MouseEvent } from "react";
 import { Link } from "react-router-dom";
 
 export interface IFavouriteCitiesProps {
-	cities: Array<string>
-	onClick: (event: MouseEvent<HTMLLIElement>) => void
-};
+  cities: Array<string>;
+  onClick: (event: MouseEvent<HTMLLIElement>) => void;
+}
 
 export class FavouriteCities extends Component<IFavouriteCitiesProps, any> {
-	render() {
-		return (
-			<div className="favourite_list">
-				<div className="favourite_list_header">Избранные города:</div>
-				{this.props.cities.length > 0 ? (
-					<ul>
-						{this.props.cities.map((city, index) => (
-							<li className="favourite_list_item" key={index} onClick={this.props.onClick}>
-								<Link to={`/forecast/${city}`}>{city}</Link>
-							</li>						
-						))}
-					</ul>
-				) : (
-					<div className="favourite_list_empty">Список пуст</div>
-				)}
-			</div>
-		)
-	};
-};
+  render() {
+    return (
+      <div className="favourite_list">
+        <div className="favourite_list_header">Избранные города:</div>
+        {this.props.cities.length > 0 ? (
+          <ul>
+            {this.props.cities.map((city, index) => (
+              <li
+                className="favourite_list_item"
+                key={index}
+                onClick={this.props.onClick}
+              >
+                <Link to={`/forecast/${city}`}>{city}</Link>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div className="favourite_list_empty">Список пуст</div>
+        )}
+      </div>
+    );
+  }
+}
